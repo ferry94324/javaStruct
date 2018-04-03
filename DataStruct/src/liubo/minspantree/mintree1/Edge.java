ab3284a8929fbaa5baf096795554674f4a8436e5
boboLiu <1357677325@qq.com>
2018-04-03 21:16:16 +0800

    最小生成树
    最小生成树在一个代码里面
    
    Co-Authored-By: liubo <liubo@users.noreply.github.com>

---
+++ DataStruct/src/liubo/minspantree/mintree1/Edge.java
@@ -0,0 +1,24 @@
+package liubo.minspantree.mintree1;
+
+public class Edge implements Comparable{  
+    public int i,j,w;  
+    public Edge(int i,int j,int w){  
+        this.i=i;  
+        this.j=j;  
+        this.w=w;  
+    }  
+      
+  
+    @Override  
+    public int compareTo(Object o) {  
+        Edge to=(Edge)o;  
+        if(this.w>to.w) return 1;  
+        else if(this.w==to.w) return 0;  
+        else return -1;  
+          
+    }  
+    @Override  
+    public String toString() {  
+        return "start="+i+"||end="+j+"||w="+w;  
+    }  
+}  
